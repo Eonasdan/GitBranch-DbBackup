242061e39b27abd61086c3b6d4cfdfa98a93d8b9
Eonasdan <jonpet2@gmail.com>
2017-12-07 10:14:27 -0500

    fixed some pathing issues

--- GitBranch-DbBackup.psm1
+++ GitBranch-DbBackup.psm1
@@ -1,78 +1,80 @@
 $DbName = ""
 $BackupPath = ""
+$SettingsPath = Join-Path $PSScriptRoot "git_db_settings.xml"
 
 function CheckParms() {
-	$result = !([string]::IsNullOrEmpty($script:BackupPath) -Or [string]::IsNullOrEmpty($script:DbName))
-	#Write-Host "Check Parms"
-	#Write-Host "BackupPath: " $script:BackupPath
-	#Write-Host "DbName: " $script:DbName
-	return $result
+    $result = !([string]::IsNullOrEmpty($script:BackupPath) -Or [string]::IsNullOrEmpty($script:DbName))
+    #Write-Host "Check Parms"
+    #Write-Host "BackupPath: " $script:BackupPath
+    #Write-Host "DbName: " $script:DbName
+    return $result
 }
 
 function GetSettings() {
-	#Write-Host "Getting Settings"
-	if (Test-Path git_db_settings.xml) {
-		[xml]$settings = Get-Content git_db_settings.xml
-		#write-host "have settings"
-		if ([string]::IsNullOrEmpty($BackupPath)) { $script:BackupPath = $settings.settings.BackupPath }
-		if ([string]::IsNullOrEmpty($DbName)) { $script:DbName = $settings.settings.DbName }
-		#Write-Host "BackupPath: " $script:BackupPath
-		#Write-Host "DbName: " $script:DbName
-	}
+    #Write-Host "Getting Settings"
+    if (Test-Path $SettingsPath) {
+        [xml]$settings = Get-Content $SettingsPath
+        #write-host "have settings"
+        if ([string]::IsNullOrEmpty($BackupPath)) { $script:BackupPath = $settings.settings.BackupPath }
+        if ([string]::IsNullOrEmpty($DbName)) { $script:DbName = $settings.settings.DbName }
+        #Write-Host "BackupPath: " $script:BackupPath
+        #Write-Host "DbName: " $script:DbName
+    }
 }
 
 function Backup-GitBranchDb($BackupPath, $DbName) {
-	$script:BackupPath = $BackupPath
-	$script:DbName = $DbName
-	if (-Not (CheckParms)) {
-		GetSettings
-		#Write-Host "BackupPath: " $script:BackupPath
-		#Write-Host "DbName: " $script:DbName
-	}
-	if (-Not (CheckParms)) {
-		$ErrorMessage = "Must provide a -DbName and -BackupPath or create a settings file as git_db_settings.xml"
-		throw [System.ArgumentException] $ErrorMessage
-	}
-	Write-Host "Doing backup"
-	$gitBranch = git branch | grep \* | cut -d ' ' -f2-
-	$toRun = 'SqlCmd -E -Q "BACKUP DATABASE [' + $script:DbName + '] TO DISK='''+ $script:BackupPath + $gitBranch + '.bak''"'
-	iex $toRun
-	#Write-Host $toRun
+    $script:BackupPath = $BackupPath
+    $script:DbName = $DbName
+    if (-Not (CheckParms)) {
+        GetSettings
+        #Write-Host "BackupPath: " $script:BackupPath
+        #Write-Host "DbName: " $script:DbName
+    }
+    if (-Not (CheckParms)) {
+        $ErrorMessage = "Must provide a -DbName and -BackupPath or create a settings file as git_db_settings.xml"
+        throw [System.ArgumentException] $ErrorMessage
+    }
+    Write-Host "Doing backup"
+    $gitBranch = git branch | grep \* | cut -d ' ' -f2-
+    $toRun = 'SqlCmd -E -Q "BACKUP DATABASE [' + $script:DbName + '] TO DISK='''+ $script:BackupPath + $gitBranch + '.bak''"'
+    iex $toRun
+    #Write-Host $toRun
 }
 
 function Retore-GitBranchDb($BackupPath, $DbName) {
-	$script:BackupPath = $BackupPath
-	$script:DbName = $DbName
-	if (-Not (CheckParms)) {
-		GetSettings
-		#Write-Host "BackupPath: " $script:BackupPath
-		#Write-Host "DbName: " $script:DbName
-	}
-	if (-Not (CheckParms)) {
-		$ErrorMessage = "Must provide a -DbName and -BackupPath or create a settings file as git_db_settings.xml"
-		throw [System.ArgumentException] $ErrorMessage
-	}
-	Write-Host "Doing backup"
-	$gitBranch = git branch | grep \* | cut -d ' ' -f2-
-	$toRun = 'SqlCmd -E -Q "RESTORE DATABASE [' + $script:DbName + '] FROM DISK='''+ $script:BackupPath + $gitBranch + '.bak''"'
-	iex $toRun
-	#Write-Host $toRun
+    $script:BackupPath = $BackupPath
+    $script:DbName = $DbName
+    if (-Not (CheckParms)) {
+        GetSettings
+        #Write-Host "BackupPath: " $script:BackupPath
+        #Write-Host "DbName: " $script:DbName
+    }
+    if (-Not (CheckParms)) {
+        $ErrorMessage = "Must provide a -DbName and -BackupPath or create a settings file as git_db_settings.xml"
+        throw [System.ArgumentException] $ErrorMessage
+    }
+    Write-Host "Doing backup"
+    $gitBranch = git branch | grep \* | cut -d ' ' -f2-
+    $toRun = 'SqlCmd -E -Q "RESTORE DATABASE [' + $script:DbName + '] FROM DISK='''+ $script:BackupPath + $gitBranch + '.bak''"'
+    iex $toRun
+    #Write-Host $toRun
 }
 
 function Save-GitBranchDbSettings($DbName, $BackupPath) {
-	Write-Host "Saving Settings"
-	$output = "git_db_settings.xml";
-	$xml = New-Object System.Xml.XmlTextWriter($output, $Null);
-	$xml.Formatting = "Indented"
-	$xml.IndentChar = " ";
-	$xml.Indentation = "1";
+    Write-Host "Saving Settings to $SettingsPath"
+    $xml = New-Object System.Xml.XmlTextWriter($SettingsPath, $Null);
+    $xml.Formatting = "Indented"
+    $xml.IndentChar = " ";
+    $xml.Indentation = "1";
 
-	# Start writing
-	$xml.WriteStartDocument();
-	$xml.WriteStartElement("settings");
-	$xml.WriteAttributeString("DbName", $DbName);
-	$xml.WriteAttributeString("BackupPath", $BackupPath);
-	$xml.Close();
+    $xml.WriteStartDocument();
+    $xml.WriteStartElement("settings");
+    $xml.WriteAttributeString("DbName", $DbName);
+    $xml.WriteAttributeString("BackupPath", $BackupPath);
+    $xml.WriteEndElement()
+    $xml.WriteEndDocument()
+    $xml.Flush()
+    $xml.Close()
 }
 
 Export-ModuleMember Backup-GitBranchDb
